b454c33e480cb5df3d523d4ba981cabfe0215b30
Ryan Elliott <ryan.elliott31@gmail.com>
2026-02-24 11:40:41 -0800

    Initial Commit

---
+++ JustOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JustOne
+//
+//  Created by Ryan Elliott on 2/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JustOne/JustOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustOneApp.swift
+//  JustOne
+//
+//  Created by Ryan Elliott on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct JustOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
